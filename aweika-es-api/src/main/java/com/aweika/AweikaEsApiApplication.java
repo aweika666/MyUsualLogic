f4b715a2f8e72582c0b6256f0de3306010968d7d
Michael <aweika666@gmail.com>
2020-08-17 14:35:08 +0800

    111

---
+++ aweika-es-api/src/main/java/com/aweika/AweikaEsApiApplication.java
@@ -0,0 +1,13 @@
+package com.aweika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AweikaEsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AweikaEsApiApplication.class, args);
+    }
+
+}
---
+++ aweika-es-api/src/test/java/com/aweika/AweikaEsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aweika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AweikaEsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
