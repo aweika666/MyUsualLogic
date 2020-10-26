545a01a4198130166352e897f6cb30f953a5cc34
QiZheFang <aweika666@gmail.com>
2020-10-26 10:58:37 +0800

    11

---
+++ mongoDB-demo/src/main/java/com/aweika/Application.java
@@ -0,0 +1,18 @@
+package com.aweika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Aweika_Fang
+ * @date: 2020/10/23
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
